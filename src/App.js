2eb40c60822d2fe6f7392e7eaf44a7fb19dc0e8f
zoiyaj <z.jesani@gmail.com>
2025-03-27 13:47:00 -0700

    added alt to logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" />
+        <img src={logo} className="App-logo img-fluid" alt="shecodes logo" />
       </header>
       Dictionary
     </div>
