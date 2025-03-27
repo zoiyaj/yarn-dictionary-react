9a64a73e156468bd7f6870154a24271532a5b203
zoiyaj <z.jesani@gmail.com>
2025-03-27 13:24:58 -0700

    Created React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" />
+      </header>
+      Dictionary
+    </div>
+  );
+}
+
+export default App;
