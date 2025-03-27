31944fb1e13e2d878b7b535a7434d8b9570419e7
zoiyaj <z.jesani@gmail.com>
2025-03-27 14:42:24 -0700

    completed app and finalized styling

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ export default function App() {
       <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Zoiya's Dictionary App</h1>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
           <small>Coded by Zoiya Jesani</small>
