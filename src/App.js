5487520aae23afa0f36073b44aaac81850291e86
zoiyaj <z.jesani@gmail.com>
2025-03-27 14:10:46 -0700

    built dictionary search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="shecodes logo" />
-      </header>
-      Dictionary
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Zoiya Jesani</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
